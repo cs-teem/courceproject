34fd953b14318a8a35730f4b2adb58a611636f6d
enayatullah <36571351+Enayatullahnaeemi@users.noreply.github.com>
2019-02-19 10:24:56 -0800

    session to the answer

--- index.php
+++ index.php
@@ -6,6 +6,13 @@ include "link.php";
 <?php
 include "top header.php";
 ?>
+
+<style type="text/css">
+#img{
+  height: 50px;
+  width: 50px;
+}
+</style>
     <!-- Main Menu area End-->
     <!-- Start Status area -->
     
@@ -27,48 +34,48 @@ include "top header.php";
                         <?php
         if(isset($_GET['ID'])){
         $id = $_GET['ID'];
-}
-        
-     ?>
-     
-          <?php
+        }
+   
       $query="select * from question";
       $sql=mysqli_query($link,$query);
       while ($row=mysqli_fetch_array($sql)) {
       
     
 
-                                  ?>                        
-                                <?php
                                 $test=$row['id'];
                                     $test= "SELECT count(*) as questions from question,answer WHERE question.id= answer.qu_id and question.id=$test";
                                     $qu=mysqli_query($link,$test);
                                     $rows = mysqli_fetch_assoc($qu);
                                 ?>
-                                <style type="text/css">
-text{
-    font-size: 12px;
 
-    font-style:;
-}
-                                </style>
+
+
+                                <style type="text/css">
+                            </style>
 
                                 <div class="jumbotron"><a class="notification" href="" >
     
     <small>Answers</small>
     <span class="badge" style="font-size: 50%"> <?php if($rows>1000){echo $rows['questions'];}else{echo"k+";}?></span>
-</a>
-                                <span><a href="answer.php?ID=<?php echo $row["id"];?>"></a></span>
-                                <span ><a href="answer.php?ID=<?php echo $row["id"];?>"><?php echo $row['description']?>?</a></span> 
-                            
+</a>                                 <span ><a href="answer.php?ID=<?php echo $row["id"];?>"></a></span>
+                             
+                             <span ><a href="answer.php?ID=<?php echo $row["id"];?>"><?php echo $row['description']?>?</a></span> 
+                       
+
+
+
+
+</div>
+
+ <?php
+}
+?>
                 
-                                </div>
+                                
 
                             
 
 
-<?php
-}?>
 
     
                       
